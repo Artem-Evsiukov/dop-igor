6269f52a0f9e44cf1d9cea650a654ac766388437
maluy <artem.evsiukov1@gmail.com>
2022-09-05 12:17:41 +0300

    dop-igot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Button} from './components/Button';
+
+type DataType = {
+    body: string
+    id: number
+    title: string
+    userId: number
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState<Array<DataType>>([])
+
+    const GetMeData = () => {
+        setData([])
+    }
+
+    useEffect(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+                .then(response => response.json())
+                .then(json => setData(json))
+        }, []
+    )
+
+    return (
+        <div className="App">
+            <Button name={'CLEAN DATA'} callBack={GetMeData}/>
+            {data.map(el => {
+                return (
+                    <li key={el.id}>
+                        <span>{el.id}</span>
+                        <span>{el.title}</span>
+                    </li>
+                )
+            })}
+            <ul>
+
+            </ul>
+        </div>
+    );
 }
 
 export default App;
